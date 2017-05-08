96552b6dc35c047072c1c914815e4fccb907700b
Steffen Exler <Steffen.Exler@gmail.com>
2017-05-08 23:47:32 +0200

    update

--- script/install.sh
+++ script/install.sh
@@ -11,8 +11,8 @@ sudo dpkg-reconfigure tzdata
 read -p "Enter new hostname [bpi-iot-ros]: " hostname
 hostname=${hostname:-bpi-iot-ros}
 
-sudo sed -i 's/127.0.0.1   localhost bpi-iot-ros/127.0.0.1   localhost $hostname/g' /etc/hosts
-sudo sed -i 's/::1         localhost bpi-iot-ros ip6-localhost ip6-loopback/::1         localhost $hostname ip6-localhost ip6-loopback/g' /etc/hosts
+sudo sed -i "s/127.0.0.1   localhost bpi-iot-ros/127.0.0.1   localhost $hostname/g" /etc/hosts
+sudo sed -i "s/::1         localhost bpi-iot-ros ip6-localhost ip6-loopback/::1         localhost $hostname ip6-localhost ip6-loopback/g" /etc/hosts
 
 sudo sh -c "echo $hostname > /etc/hostname"
 sudo hostname -F /etc/hostname
