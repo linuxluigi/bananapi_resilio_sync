c419b350ba44b35bf4a16ef9ae2f137fcc0379a8
Steffen Exler <Steffen.Exler@gmail.com>
2017-05-09 00:12:58 +0200

    update

---
+++ cron/hourly/ntpdate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# update systemtime from ntp server
+ntpdate -s 0.de.pool.ntp.org
--- script/install.sh
+++ script/install.sh
@@ -17,9 +17,23 @@ sudo sed -i "s/::1         localhost bpi-iot-ros ip6-localhost ip6-loopback/::1 
 sudo sh -c "echo $hostname > /etc/hostname"
 sudo hostname -F /etc/hostname
 
+# syncron ntp time hourly
+sudo apt-get -y install ntp ntpdate
+ntpdate -s 0.de.pool.ntp.org
+
+sudo sh -c "echo '#!/bin/bash' > /etc/cron.hourly/ntpdate"
+sudo sh -c "echo 'ntpdate -s 0.de.pool.ntp.org' >> /etc/cron.hourly/ntpdate"
+
+
+# activate auto install security updates
+sudo apt-get -y install unattended-upgrades
+sudo sh -c "echo 'APT::Periodic::Update-Package-Lists "1";' > /etc/apt/apt.conf.d/10periodic"
+sudo sh -c "echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/10periodic"
+sudo sh -c "echo 'APT::Periodic::Download-Upgradeable-Packages "1";' >> /etc/apt/apt.conf.d/10periodic"
+sudo sh -c "echo 'APT::Periodic::AutocleanInterval "7";' >> /etc/apt/apt.conf.d/10periodic"
+sudo sh -c "echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/10periodic"
+
 # secure
-# auto update
-# set timezone
 # backup
 
 # add cronejob
